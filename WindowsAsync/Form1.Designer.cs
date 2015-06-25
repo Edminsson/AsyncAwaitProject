ed25eca514a4d51dfee836c948d6749567684df6
Axel Aguilar <abc709@hotmail.com>
2015-06-25 14:52:26 +0200

    Lägger till Windowsprojekt

---
+++ WindowsAsync/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsAsync
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startBtn = new System.Windows.Forms.Button();
+            this.resultatTxt = new System.Windows.Forms.TextBox();
+            this.skrivBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(107, 33);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "Starta";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // resultatTxt
+            // 
+            this.resultatTxt.Location = new System.Drawing.Point(32, 142);
+            this.resultatTxt.Multiline = true;
+            this.resultatTxt.Name = "resultatTxt";
+            this.resultatTxt.Size = new System.Drawing.Size(212, 146);
+            this.resultatTxt.TabIndex = 1;
+            // 
+            // skrivBtn
+            // 
+            this.skrivBtn.Location = new System.Drawing.Point(107, 62);
+            this.skrivBtn.Name = "skrivBtn";
+            this.skrivBtn.Size = new System.Drawing.Size(75, 23);
+            this.skrivBtn.TabIndex = 2;
+            this.skrivBtn.Text = "Skriv medd";
+            this.skrivBtn.UseVisualStyleBackColor = true;
+            this.skrivBtn.Click += new System.EventHandler(this.skrivBtn_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ej responsiv";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 300);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.skrivBtn);
+            this.Controls.Add(this.resultatTxt);
+            this.Controls.Add(this.startBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.TextBox resultatTxt;
+        private System.Windows.Forms.Button skrivBtn;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsAsync/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsAsync
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Applikationen ser ut att vara responsiv under awaiten.
+        private async void startBtn_Click(object sender, EventArgs e)
+        {
+            //Observera att Click-hanteraren måste vara async för att det ska funka
+            //Jag kan inte anropa GetResult som jag gjorde i konsolprogrammet.
+            //var textTask = GetResult();
+            //resultatTxt.Text = textTask.Result;
+            var texten = await GetResult();
+            resultatTxt.Text = texten;
+        }
+
+        public async Task<string> GetResult()
+        {
+            HttpClient client = new HttpClient();
+            Task<string> getStringTask = client.GetStringAsync("http://msdn.microsoft.com");
+            string urlContents = await getStringTask;
+            return urlContents;
+        }
+
+        private void skrivBtn_Click(object sender, EventArgs e)
+        {
+            resultatTxt.Text += "Meddelande";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+
+            client.Headers.Add ("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+
+            Stream data = client.OpenRead("http://msdn.microsoft.com");
+            StreamReader reader = new StreamReader (data);
+            string s = reader.ReadToEnd ();
+            data.Close ();
+            reader.Close ();
+
+            resultatTxt.Text = s;
+        }
+    }
+}
